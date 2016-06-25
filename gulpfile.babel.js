335a745488062c7377cc9059c70c9f2e60eb3ff8
David Salgado <david@digitalronin.com>
2016-06-25 19:38:29 +0100

    add gulp watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,10 +4,18 @@ import gulp from "gulp";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 
-gulp.task("default", () => {
+gulp.task("default", ["transpile"]);
+
+gulp.task("transpile", () => {
+
   return browserify("src/app.js")
     .transform("babelify")
     .bundle()
     .pipe(source("application.js"))
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest("dist"));
+
+});
+
+gulp.task("watch", ["transpile"], () => {
+  gulp.watch("src/**/*", ["transpile"]);
 });
